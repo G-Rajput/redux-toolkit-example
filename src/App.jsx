2e20612c0a9b6cba97f6e4a9ef73eb21194267c0
Govind Rajput <govindrajput9819@gmail.com>
2024-04-15 18:13:13 +0530

    Header Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Header from "./components/header/Header";
 
 function App() {
-  return <div>Redux ToolKit Example</div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
